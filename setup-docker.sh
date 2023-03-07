ee2ecd6c4068408a6b362c893dd2775164f11ffb
Martin Dimitrov <52877024+mark79-github@users.noreply.github.com>
2023-03-07 10:15:40 +0200

    Create setup-docker.sh

---
+++ setup-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "*** Adding docker repository ..."
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo "*** Install docker ..."
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+echo "*** Adding vagrant user to the docker group, to be able to run docker command without the need to use sudo ..."
+sudo usermod -aG docker vagrant
+
+echo "*** Enabling docker daemon to start on boot ..."
+sudo systemctl enable docker
+
+echo "*** Starting the docker service ..."
+sudo systemctl start docker
